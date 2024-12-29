410d66f2dae8145c56cce28b2b8cdb48474f89e5
Paz <50664967+TwoWaySix@users.noreply.github.com>
2024-12-29 13:56:59 +0100

    added markdown rendering via showdown

--- frontend/src/views/NoteView.vue
+++ frontend/src/views/NoteView.vue
@@ -5,40 +5,90 @@ import { onMounted, ref, type Ref } from 'vue'
 
 import { useRoute } from 'vue-router'
 
+import * as showdown from 'showdown'
+
+const converter = new showdown.Converter({
+  tasklists: true,
+})
+
+const baseURL: string = `${window.location.origin}/note`
+
 const route = useRoute()
 
 const noteRepository = new NoteRepository()
 
 const note: Ref<Note | null> = ref(null)
 
-const noteText: Ref<string> = ref('')
+const noteTextMd: Ref<string> = ref('')
+
+const isEditMode: Ref<boolean> = ref(false)
 
 onMounted(async () => {
+  await fetchData()
+})
+
+async function fetchData() {
+  isEditMode.value = false
   const filePath = route.params.path.join('/')
   note.value = await noteRepository.getByPath(filePath)
+  updateMarkdown()
+}
 
-  const pattern = /\[.*\]/
-  const matches = note.value.text.match(pattern)
-  console.log(matches)
+function toggleEditMode() {
+  isEditMode.value = !isEditMode.value
+}
 
-  noteText.value = `${note.value.text}`
-  matches?.forEach((m) => {
-    console.log(m)
-    const replacement = m.replace('[[', '').replace(']]', '')
-    const parts = replacement.split('/')
-    const fileName = parts[parts.length - 1].replace('.md', '')
-    console.log(fileName)
-    console.log(replacement)
-    noteText.value = noteText.value.replace(m, `<a href="/note/${replacement}">${fileName}</a>`)
-  })
-})
+function updateMarkdown() {
+  noteTextMd.value = converter.makeHtml(note.value?.text.replace('<BASEURL>', baseURL))
+}
+
+async function saveNote() {}
 </script>
 
 <template>
   <main v-if="note">
-    <h2>
-      {{ note?.path }}
-    </h2>
-    <p v-html="noteText"></p>
+    <header>
+      <h2 class="title">Displaying {{ note?.path }}</h2>
+      <template v-if="isEditMode">
+        <div class="button-row">
+          <button @click="saveNote()">Save</button>
+          <button @click="fetchData()">Cancel</button>
+        </div>
+      </template>
+      <template v-else>
+        <button @click="toggleEditMode()">Edit</button>
+      </template>
+    </header>
+
+    <div class="markdown">
+      <template v-if="isEditMode">
+        <textarea v-model="note.text"></textarea>
+      </template>
+      <template v-else>
+        <div v-html="noteTextMd"></div>
+      </template>
+    </div>
   </main>
 </template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 2px solid var(--color-light);
+  padding: 0.5rem 0;
+  margin-bottom: 1rem;
+}
+
+textarea {
+  background-color: var(--color-background);
+  width: 100%;
+  height: 100vh;
+  color: var(--color-text);
+}
+
+.title {
+  color: var(--color-light);
+  font-weight: bold;
+}
+</style>
--- testdata/Something.md
+++ testdata/Something.md
@@ -4,4 +4,46 @@ Something something
 
 ## Links to other files
 
-This leads to [[Deep/Something Deep.md]], if everything works
+A link to [Something Deep.md](<BASEURL>/Top%20Level%201/Deep/Something%20Deep.md)
+
+## Code
+
+This even has code like `python3 -m http.server` or something longer:
+
+```
+def plus_one(number: int) -> int:
+    return number + 1
+```
+
+### Styling
+
+How about _Something Italic_, **Something Bold** or so **_both of it_** using the \* character in Markdown.
+
+### Something else nested
+
+In the words of Abraham Lincoln:
+
+> Pardon my french
+
+Or something along those lines.
+
+## Lists are useful
+
+### So how about one
+
+- First item
+  - Second level
+  - Also second level
+    - Something on the third level
+- Second item
+- Third item
+
+### Or with numbers
+
+1. Whatever
+2. However
+
+### Checkboxes are even better
+
+- [x] Important thing to do
+- [ ] Less important thing to do
